3703bb0fb40cd39bd8902aaaa01eeb00852b3110
khilikegor <khilik.egor@gmail.com>
2024-12-29 23:58:43 +0300

    fix

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,7 +10,7 @@ var DB *gorm.DB // Глобальная переменная для хранен
 
 func Connect() {
     var err error
-    dsn := "host=aviasales-psql-1 user=admin password=admin dbname=aviasales_bot_bd sslmode=disable"
+    dsn := "host=psql user=admin password=admin dbname=aviasales_bot_bd sslmode=disable"
     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Printf("Failed to connect to the database with DSN: %s", dsn)
